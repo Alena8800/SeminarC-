ac2513fe5849654a8812331d8eb4044878afe744
Alena <alena8800@mail.ru>
2023-02-11 17:23:04 +0300

    task1

---
+++ task7/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №7. Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе показывает
+// последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int lastNumber = number % 10;
+    System.Console.WriteLine("Последнее число: " + lastNumber);
+}
+else
+{
+    System.Console.WriteLine("Неправильный номер: ");
+}
