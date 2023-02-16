ec9569bd20a00f957105ed4261961fd8f8ced14d
Alena <alena8800@mail.ru>
2023-02-16 19:35:37 +0300

    task0

---
+++ task0/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает
+//  наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int num = new Random().Next(10,100);
+Console.WriteLine("Было сгенерировано "+num);
+Console.WriteLine($"Было сгенерировано число {num}");
+int x1 = num / 10;
+int x2 = num % 10;
+if (x1>x2)
+Console.WriteLine(x1);
+else if (x1<x2)
+Console.WriteLine(x2);
+else
+Console.WriteLine("обе цифры этого числа равны");
