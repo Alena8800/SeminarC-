95aad441d64e6b18ec56f0b9fac9c175bd7283d9
Alena <alena8800@mail.ru>
2023-02-18 12:18:25 +0300

    HomeWork_task15

---
+++ HomeWork_task15/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+ Console.WriteLine("Введите номер дня");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+	Console.Write("Понедельник - не выходной");
+}
+else if (day == 2)
+{
+	Console.Write("Вторник - не выходной");
+}
+else if (day == 3)
+{
+	Console.Write("Среда - не выходной");
+}
+else if (day == 4)
+{
+	Console.WriteLine("Четверг - не выходной");
+}
+else if (day == 5)
+{
+	Console.WriteLine("Пятница - не выходной");
+}
+else if (day == 6)
+{
+	Console.WriteLine("Суббота - выходной");
+}
+else if (day == 7)
+{
+	Console.WriteLine("Воскресенье - выходной");
+}
+else 
+{
+	Console.WriteLine("Введены некоректные значения.");
+}
+
