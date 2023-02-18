6ffa29c72e7bdfcadaa39e15cc8db6feab72325c
Alena <alena8800@mail.ru>
2023-02-18 12:20:27 +0300

    HomeWork_task13

---
+++ HomeWork_task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.Через строку решать нельзя.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2=num;
+int count=0;
+while (num > 0)
+{
+count++;
+num = num / 10;
+}
+Console.WriteLine($"Разрядность введенного числа {count}");
+
+string res="";
+for (int i=1;i<=count;i++)
+{
+if (i!=count-1)
+res = num2%10 + res ;
+num2=num2/10;
+}
+
+Console.WriteLine(res);
