b29d4ad68c6a1348343cf6f4895837beea302d2b
Alena <alena8800@mail.ru>
2023-03-04 11:31:11 +0300

    HomeWork_task19

--- HomeWork_task19/Program.cs
+++ HomeWork_task19/Program.cs
@@ -5,28 +5,29 @@
 // 12821 -> да
 // 23432 -> да
 
-void CheckThePalindrome(string palindrome)
+void CheckThePalindrome(int number, int palindrome1, int palindrome2, int palindrome3, int palindrome4)
 {
-    int number = palindrome.Length; 
-    if (number == 5)
+    if (number >= 10000 && number <= 99999)
     {
-        if (palindrome[0] == palindrome[4] && palindrome[1] == palindrome[3])
+        Console.WriteLine();
+        if (palindrome1 == palindrome2 && palindrome3 == palindrome4)
         {
             Console.WriteLine("Палиндром");
         }
-
         else
         {
-            Console.WriteLine("Не является палиндромом");
+            Console.WriteLine("Не палиндром");
         }
     }
     else
     {
-        Console.WriteLine("Ввели неверное число");
+        Console.WriteLine("Введено неверное число");
     }
 }
-
 Console.WriteLine("Введите пятизначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-string palindromeLength = Convert.ToString(number);
-CheckThePalindrome("");
+int palindrome1 = number % 10;
+int palindrome2 = number / 10000;
+int palindrome3 = number % 100 / 10;
+int palindrome4 = number / 1000 % 10;
+CheckThePalindrome(number, palindrome1, palindrome2, palindrome3, palindrome4);
