f03e63b861daaa0c5f167242a8d1230fae262a07
Alena <alena8800@mail.ru>
2023-03-04 13:07:20 +0300

    task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,12 +1,18 @@
 ﻿// сколько цифр в числе
 
-Console.WriteLine("Введите любое натуральное число");
+int Lenght(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+System.Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-
-int[] array = {number};
-int n = array.Length;
-System.Console.WriteLine($"{n}");
-
+System.Console.WriteLine($"Цифр в числе {Lenght(number)}");
 
 
 
